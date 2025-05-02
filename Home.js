c8879b618b283d3eacdcfb14e5e858b6efd71880
maihuynh.github.io@gmail.com <quemaihuynh12330@gmail.com>
2025-05-02 22:02:55 +0300

    Testing 1

---
+++ Home.js
@@ -0,0 +1,8 @@
+<script>
+    const menuToggle = document.getElementById("menu-toggle");
+    const navbar = document.querySelector(".navbar");
+
+    menuToggle.addEventListener('click', () ={">"} {
+        navbar.classList.toggle('active'),  // Toggle 'active' class on navbar
+    });
+</script>
